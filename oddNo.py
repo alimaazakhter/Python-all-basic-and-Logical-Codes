448f76185795460e79ac301434f8b411e8e6e75e
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-07 11:34:33 +0530

    Add files via upload
    Sum of first ten odd number using while loop

---
+++ oddNo.py
@@ -0,0 +1,10 @@
+# #SUM OF FIRST TEN ODD NUMBER
+# sum = 0
+# n = 0
+# while n<=20:
+#     if n % 2 !=0:
+#         sum += n
+#     n +=1    
+# print("odd number of first ten odd number is : ", sum)   
+
+print(type("pYTHON"))
