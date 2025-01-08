08c7e4696c668aadf2bf1f767a1c3800285df975
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-08 16:55:40 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,14 @@
+#string
+a = "Why fit in, When you are born to stand out"
+print(len(a))
+print(a.count("o"))
+
+x = a.lower()
+print(x)
+
+y = a.upper()
+print(y)
+
+print(a.title())
+
+print(a.index("fit in"))
